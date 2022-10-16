c3f1813a1b54b956b4aa4df495734103bffe39a7
Kidüs Michael <kidusmichael08@gmail.com>
2022-10-16 16:32:51 -0700

    Start from scratch

--- ike-frontend/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import App from './App';
-import ReactDOM from "react-dom";
-
-test('Main app renders without crashing', () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
--- ike-frontend/src/Components/Navbar/MenuItems.js
+++
@@ -1,7 +0,0 @@
-export const MenuItems = [
-    {
-        title: 'Dashboard',
-        url: '/dashboard',
-        cName: 'nav-links-mobile'
-    }
-]
