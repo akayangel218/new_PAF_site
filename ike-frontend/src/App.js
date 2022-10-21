c2e2c981456464f1c16207f799d57526b1ce7055
Kidüs Michael <kidusmichael08@gmail.com>
2022-10-20 19:40:59 -0700

    Added intro text.
    Unit bubbles go next

--- ike-frontend/src/App.js
+++ ike-frontend/src/App.js
@@ -7,7 +7,24 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className='title'> Aloha </div>
-      <div className='intro_desc'> Welcome to the Waikalua Loko I'a interactive platform. </div>
+      <div className='textbox'> 
+        <span className='text'>
+          Welcome to the Waikalua Loko I'a interactive platform.
+        </span> 
+        <br></br>
+        <span className='text'>
+          The Information shared will support us in continuing to foster kinship and kuleana between kanaka and ʻāina.
+        </span> 
+        <br></br>
+        <span className='text'>
+          Please select a unit to begin.
+        </span>
+        <br></br>
+        <br></br>
+        <span className='text'>
+          Mahalo nui.
+        </span> 
+      </div>
     </div>
   );
 }
